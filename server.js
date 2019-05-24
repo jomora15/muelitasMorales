fe3e7195e706c88ec54976ded0d5ced96baf4a93
jomora15 <jomora15@hotmail.com>
2019-05-23 19:12:09 -0500

    updates to deploy heroku

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-//Install express server    
+//Install express server
 const express = require('express');
-
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory    
-app.use(express.static(__dirname + '/dist'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/muelitasMorales'));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+
+    res.sendFile(path.join(__dirname + '/dist/muelitasMorales/index.html'));
 });
 
-// Start the app by listening on the default Heroku port    
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
